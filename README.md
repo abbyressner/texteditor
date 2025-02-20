2f945cf0de3a04e340bc2374913991c07f76c970
Abby Ressner <abbyressner@gmail.com>
2025-02-19 23:33:55 -0600

    implement TextEditor and add to README.md

--- README.md
+++ README.md
@@ -5,10 +5,11 @@
 ## Resources Used
 
 + Java 21.0.6
-+ Apache Maven v?
++ Apache Maven 3.9.9
 + Visual Studio Code
-+ Lab 5: Testing Frameworks - 
++ [Project 4 instuctions page](https://osera.cs.grinnell.edu/ttap/data-structures-labs/text-editor.html)
++ Lab 5: Testing Frameworks - for help structuring my test suites
 + [Stack Overflow](https://stackoverflow.com/questions/50091802/vs-code-commit-undo) - for instructions on how to undo a commit
-+ ...
++ 
 
 ## Changelog
--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -1,5 +1,17 @@
 package edu.grinnell.csc207.texteditor;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
 /**
  * The driver for the TextEditor Application.
  */
@@ -7,16 +19,69 @@ public class TextEditor {
 
     /**
      * The main entry point for the TextEditor application.
+     *
      * @param args command-line arguments.
+     * @throws IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.err.println("Usage: java TextEditor <filename>");
             System.exit(1);
         }
 
-        // TODO: fill me in with a text editor TUI!
         String path = args[0];
-        System.out.format("Loading %s...\n", path);
+        GapBuffer buf = new GapBuffer();
+        Path filePath = Paths.get(path);
+
+        if (Files.exists(filePath) && Files.isRegularFile(filePath)) {
+            String fileContents = Files.readString(filePath);
+            char[] contents = fileContents.toCharArray();
+            for (char ch : contents) {
+                buf.insert(ch);
+            }
+        }
+
+        DefaultTerminalFactory factory = new DefaultTerminalFactory();
+        Screen screen = factory.createScreen();
+
+        screen.startScreen();
+        boolean isRunning = true;
+        while (isRunning) {
+            drawBuffer(buf, screen);
+            KeyStroke stroke = screen.readInput();
+            KeyType key = stroke.getKeyType();
+            if (key == KeyType.Character) {
+                buf.insert(stroke.getCharacter());
+            } else if (key == KeyType.Backspace) {
+                buf.delete();
+            } else if (key == KeyType.ArrowLeft) {
+                buf.moveLeft();
+            } else if (key == KeyType.ArrowRight) {
+                buf.moveRight();
+            } else if (key == KeyType.Escape) {
+                isRunning = false;
+            }
+        }
+
+        Files.writeString(filePath, buf.toString());
+        screen.stopScreen();
+    }
+
+    /**
+     * Renders the contents of the buffer.
+     *
+     * @param buf the text buffer holding the document's contents.
+     * @param screen the screen to draw to.
+     * @throws IOException
+     */
+    private static void drawBuffer(GapBuffer buf, Screen screen) throws IOException {
+        screen.clear();
+        String text = buf.toString();
+        for (int i = 0; i < text.length(); i++) {
+            screen.setCharacter(0, i, TextCharacter.fromCharacter(text.charAt(i))[0]);
+        }
+        int cursorPos = buf.getCursorPosition();
+        screen.setCursorPosition(new TerminalPosition(0, cursorPos));
+        screen.refresh();
     }
 }
